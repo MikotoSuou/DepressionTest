a815b1059c92605e7b7e0e654a90c26f08deff42
bj.geronimo <bj@risingtide.ph>
2022-05-20 16:27:24 +0800

    created depression test and result activity

--- app/src/main/java/com/mobile/depressiontest/MainActivity.java
+++ app/src/main/java/com/mobile/depressiontest/MainActivity.java
@@ -2,13 +2,26 @@ package com.mobile.depressiontest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mobile.depressiontest.activities.DepressionTestActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), DepressionTestActivity.class)));
     }
 }
